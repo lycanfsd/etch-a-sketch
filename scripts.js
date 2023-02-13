ee8c30229cd803bc7d6ed8053bde08358fb01c60
Lycan <lycanfsd@gmail.com>
2023-02-13 17:58:24 -0600

    Create function to change grid size and function to clear grid
    Also made small style changes

--- scripts.js
+++ scripts.js
@@ -1,7 +1,23 @@
+const DEFAULT_GRID_SIZE = 16;
+
+//grid elements
 let gridContainer = document.querySelector(".gridContainer");
+let gridSizeSlider = document.querySelector('#gridSizeSlider');
+let displayedGridSize = document.querySelector('#displayedGridSize');
 
+let currentGridSize = DEFAULT_GRID_SIZE;
+let columns;
+let rows;
 
-createGrid(16, 16);
+//button elements
+const clearButton = document.querySelector('#clearButton');
+clearButton.addEventListener('click', clearGrid());
+
+//Change grid size value in HTML
+gridSizeSlider.onchange = (e) => updateDisplayedGridSize(e.target.value);
+gridSizeSlider.onchange = (e) => changeGridSize(e.target.value);
+
+createGrid(currentGridSize, currentGridSize);
 
 // Take input for grid dimensions and create grid
 
@@ -17,15 +33,40 @@ function createGrid(columns, rows) {
     }
 }
 
+function updateDisplayedGridSize(size) {
+    clearGrid();
+    displayedGridSize.innerHTML = `Grid Size: ${size} x ${size}` 
+}
+
+function setCurrentGridSize(size) {
+    currentGridSize = size;
+}
+
+function changeGridSize(size) {
+    setCurrentGridSize(size);
+    updateDisplayedGridSize(size);
+    reloadGrid();
+}
+
+function reloadGrid() {
+    clearGrid();
+    createGrid(currentGridSize, currentGridSize);
+}
+
+console.log(currentGridSize);
+
 // When button is pressed, clear the grid
 
 function clearGrid() {
-
+    gridContainer.innerHTML = '';
 }
 
 function standardGridProperties(cell) {
-    cell.style.backgroundColor = 'orange';
+    cell.style.backgroundColor = 'white';
     cell.style.border = 'solid black 1px';
 }
 
-//Grid wont make rows and columns. Work on it.
+console.log(gridSizeSlider.value);
+
+//Yesterday created a clear function. Need to add a pen tool function
+// and function for other buttons. Work on this!
